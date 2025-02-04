4ee20f98f7c42f883a22634e89a61150cb9905b6
jatin jain <jainjatin748@gmail.com>
2025-02-04 15:00:48 +0530

    kj

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,7 +1,14 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-
+  {
+    path: 'notes/edit/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      // Fetch or define IDs for prerendering
+      return [{ id: '1' }, { id: '2' }, { id: '3' }]; // Example IDs
+    }
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender // Keep Prerendering for other pages
