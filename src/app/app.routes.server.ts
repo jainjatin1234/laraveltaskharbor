4397a97b2c161482dcc74b315eca34debce54046
jatin jain <jainjatin748@gmail.com>
2025-02-04 13:59:30 +0530

    jh

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,10 +1,7 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: 'notes/edit/:id',
-    renderMode: RenderMode.SSR // Use SSR for dynamic pages
-  },
+
   {
     path: '**',
     renderMode: RenderMode.Prerender // Keep Prerendering for other pages
